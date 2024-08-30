ca477f36cf51937de4715fd801b957662bd3981d
Rafael Cremasco Lacerda <rafaelcremasco@hotmail.com>
2024-08-30 20:14:53 -0300

    Solução para DIO - Trilha .NET - Programação orientada a objetos - XP Investimentos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "27 9999-9999", modelo: "XWT-10", iMEI: "11111111111", memoria: 128);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("XP Investimentos");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "27 8888-8888", modelo: "15", iMEI: "7777777777", memoria: 256);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Nubank");
